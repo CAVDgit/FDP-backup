f838d11793944c9ead33b5bdb03e0217b2cc1ca0
CAVDgit <147713659+CAVDgit@users.noreply.github.com>
2025-03-31 10:24:25 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CRON_EXPRESSION="${FDP_CRON_SCHEDULE:-0 22 * * *}"
-echo "$CRON_EXPRESSION python /app/fdp_backup.py >> /app/backup/backup.log 2>&1" > /app/fdp.cron
-
-echo "🕒 Using cron schedule: $CRON_EXPRESSION"
-cat /app/fdp.cron
-
-exec /usr/local/bin/supercronic /app/fdp.cron
